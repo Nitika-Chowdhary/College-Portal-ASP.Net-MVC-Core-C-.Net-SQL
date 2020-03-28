6bba0d9f1713fc1ef79cf449d850c71ba8581540
Nitika Chowdhary <nitikachowdhary15@gmail.com>
2020-03-28 00:58:01 -0400

    Starting Building from Scratch

---
+++ Humber College/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Humber_College.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
